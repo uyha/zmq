9f48fa5ac520104cc882c34797d9c5c85cde7afc
Uy Ha <hchanuy@gmail.com>
2024-10-26 12:44:49 +0000

    build `libzmq.a` on when necessary

--- build.zig
+++ build.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 fn buildLibzmq(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) *std.Build.Module {
     const upstream = b.dependency("upstream", .{});
+
     const triple = target.result.linuxTriple(b.allocator) catch @panic("OOM");
     const cc = b.fmt(
         "-DCMAKE_C_COMPILER='{s};cc;--target={s}'",
@@ -17,49 +18,39 @@ fn buildLibzmq(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.bu
     const cmake_configure = b.addSystemCommand(&.{
         cmake,
         "-G=Ninja",
-        "-B",
-        upstream.path("build").getPath(b),
-        "-S",
-        upstream.path("").getPath(b),
         cc,
         cxx,
+        "-DWITH_TLS=OFF",
         "-DWITH_PERF_TOOL=OFF",
         "-DZMQ_BUILD_TESTS=OFF",
         "-DENABLE_CPACK=OFF",
         "-DENABLE_DRAFTS=ON",
+        "-Wno-dev",
         build_type,
+        "-S",
     });
+    cmake_configure.addDirectoryArg(upstream.path(""));
+    cmake_configure.addArg("-B");
+    const build_dir = cmake_configure.addOutputDirectoryArg("build");
 
-    const cmake_build = b.addSystemCommand(&.{
-        cmake,
-        "--build",
-        upstream.path("build").getPath(b),
-    });
-    cmake_build.step.dependOn(&cmake_configure.step);
-
-    const lib_file = b.addInstallFile(
-        upstream.path("build/lib/libzmq.a"),
-        "libzmq.a",
-    );
-    lib_file.step.dependOn(&cmake_build.step);
+    const cmake_build = b.addSystemCommand(&.{ cmake, "--build" });
+    cmake_build.addDirectoryArg(build_dir);
 
     const library = b.addTranslateC(.{
         .root_source_file = upstream.path("include/zmq.h"),
         .target = target,
         .optimize = optimize,
     });
-    library.step.dependOn(&lib_file.step);
+    library.step.dependOn(&cmake_build.step);
 
     const module = b.addModule("libzmq", .{
         .root_source_file = library.getOutput(),
         .target = target,
         .optimize = optimize,
         .link_libc = true,
+        .link_libcpp = true,
     });
-    module.addLibraryPath(lib_file.source.dirname());
-    module.linkSystemLibrary("zmq", .{
-        .use_pkg_config = .no,
-    });
+    module.addObjectFile(build_dir.path(b, "lib/libzmq.a"));
 
     return module;
 }
@@ -68,5 +59,24 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = buildLibzmq(b, target, optimize);
+    const libzmq_module = buildLibzmq(b, target, optimize);
+
+    const zmq = b.addStaticLibrary(.{
+        .name = "zmq",
+        .root_source_file = b.path("src/zmq.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(zmq);
+
+    const zmq_test = b.addTest(.{
+        .root_source_file = b.path("test/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    zmq_test.root_module.addImport("libzmq", libzmq_module);
+    const zmq_test_run = b.addRunArtifact(zmq_test);
+
+    const test_step = b.step("test", "Run all the tests");
+    test_step.dependOn(&zmq_test_run.step);
 }
