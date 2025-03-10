f73e8b007604ca1bb6a65726aeb43ca9953e4e04
Uy Ha <hchanuy@gmail.com>
2025-03-10 09:08:41 +0000

    generate both get and set options for context

--- zmq/tools/gen-ctxopt.py
+++ zmq/tools/gen-ctxopt.py
@@ -2,13 +2,20 @@ from urllib import request
 
 from parsel import Selector
 
+print("""const zmq = @import("libzmq");""")
+
+# -------------------------------------Set Options-------------------------------------
+set_types = {
+    "bool": ["blocky", "zero_copy_recv", "ipv6"],
+    "[:0]const u8": ["thread_name_prefix"],
+}
+
 url = "https://libzmq.readthedocs.io/en/latest/zmq_ctx_set.html"
 with request.urlopen(url) as resp:
     sel = Selector(resp.read().decode())
 
-bool_options = ["blocky", "zero_copy_recv", "ipv6"]
 
-print("pub const Option = enum(c_int) {")
+print("pub const SetOption = enum(c_int) {")
 for text in sel.xpath("//div[@class='sect2']/h3/text()"):
     raw = text.get()
     assert raw is not None
@@ -16,15 +23,46 @@ for text in sel.xpath("//div[@class='sect2']/h3/text()"):
 
     name, _ = raw.split(":")
     name = name.strip()
-    zig = name.lstrip("ZMQ_").lower()
+    assert name.startswith("ZMQ_")
+    zig = name.removeprefix("ZMQ_").lower()
     print(f"    {zig} = zmq.{name},")
 
 print("};")
+print("pub fn SetOptionType(option: SetOption) type {")
+print("    return switch (option) {")
+for t, opts in set_types.items():
+    print(f"        {", ".join(f".{opt}" for opt in opts)} => {t},")
+print("        else => c_int,")
+print("    };")
+print("}")
 print("")
 
-print("pub fn OptionType(option: Option) type {")
+# -------------------------------------Get Options-------------------------------------
+
+get_types = {
+    "bool": ["blocky", "zero_copy_recv", "ipv6"],
+    "[:0]u8": ["thread_name_prefix"],
+}
+url = "https://libzmq.readthedocs.io/en/latest/zmq_ctx_get.html"
+with request.urlopen(url) as resp:
+    sel = Selector(resp.read().decode())
+print("pub const GetOption = enum(c_int) {")
+for text in sel.xpath("//div[@class='sect2']/h3/text()"):
+    raw = text.get()
+    assert raw is not None
+    assert isinstance(raw, str)
+
+    name, _ = raw.split(":")
+    name = name.strip()
+    assert name.startswith("ZMQ_")
+    zig = name.removeprefix("ZMQ_").lower()
+    print(f"    {zig} = zmq.{name},")
+
+print("};")
+print("pub fn GetOptionType(option: GetOption) type {")
 print("    return switch (option) {")
-print(f"        {", ".join(f".{opt}" for opt in bool_options)} => bool,")
+for t, opts in get_types.items():
+    print(f"        {", ".join(f".{opt}" for opt in opts)} => {t},")
 print("        else => c_int,")
 print("    };")
 print("}")
