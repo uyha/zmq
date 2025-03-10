e3698d7f5dc85ff18caea50e0564c93b80b8ad33
Uy Ha <hchanuy@gmail.com>
2025-03-10 08:13:17 +0000

    add set context option

--- zmq/src/Context.zig
+++ zmq/src/Context.zig
@@ -22,3 +22,40 @@ pub inline fn init() InitError!Self {
 pub fn deinit(self: Self) void {
     _ = zmq.zmq_ctx_term(self.handle);
 }
+
+pub const Option = enum(c_int) {
+    blocky = zmq.ZMQ_BLOCKY,
+    io_threads = zmq.ZMQ_IO_THREADS,
+    thread_sched_policy = zmq.ZMQ_THREAD_SCHED_POLICY,
+    thread_priority = zmq.ZMQ_THREAD_PRIORITY,
+    thread_affinity_cpu_add = zmq.ZMQ_THREAD_AFFINITY_CPU_ADD,
+    thread_affinity_cpu_remove = zmq.ZMQ_THREAD_AFFINITY_CPU_REMOVE,
+    thread_name_prefix = zmq.ZMQ_THREAD_NAME_PREFIX,
+    ax_msgsz = zmq.ZMQ_MAX_MSGSZ,
+    ero_copy_recv = zmq.ZMQ_ZERO_COPY_RECV,
+    ax_sockets = zmq.ZMQ_MAX_SOCKETS,
+    ipv6 = zmq.ZMQ_IPV6,
+};
+
+pub fn OptionType(option: Option) type {
+    return switch (option) {
+        .blocky, .zero_copy_recv, .ipv6 => bool,
+        else => c_int,
+    };
+}
+
+pub const SetError = error{ OptionInvalid, Unexpected };
+pub fn set(self: Self, comptime option: Option, value: OptionType(option)) SetError!void {
+    const Value = @TypeOf(value);
+    const raw_value: c_int = switch (Value) {
+        bool => @intFromBool(value),
+        c_int => value,
+        else => @compileError("Unrecognized type: " ++ @typeName(Value)),
+    };
+    if (zmq.zmq_ctx_set(self.handle, option, raw_value) == -1) {
+        return switch (c._errno().*) {
+            zmq.EINVAL => SetError.OptionInvalid,
+            else => SetError.Unexpected,
+        };
+    }
+}
